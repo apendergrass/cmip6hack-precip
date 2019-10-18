dadbe6eb7eacb4cc748296ac32e0780e4a5224a3
apendergrass <apgrass@ucar.edu>
2019-10-18 03:43:41 -0600

    getting util.py back up to speed with master

--- notebooks/util.py
+++ notebooks/util.py
@@ -5,6 +5,7 @@
 
 import re
 import socket
+import intake
 
 def is_ncar_host():
     """Determine if host is an NCAR machine."""
@@ -13,3 +14,10 @@ def is_ncar_host():
     return any([re.compile(ncar_host).search(hostname) 
                 for ncar_host in ['cheyenne', 'casper', 'hobart']])
 
+def load_data_catalog():
+    """Load data on either NCAR or pangeo machine."""
+    if is_ncar_host():
+        col = intake.open_esm_datastore("../catalogs/glade-cmip6.json")
+    else:
+        col = intake.open_esm_datastore("../catalogs/pangeo-cmip6.json")
+    return col
